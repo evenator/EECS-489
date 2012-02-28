61126798886665cabb4c13428b154436b499de9a
Ed Venator <esv@case.edu>
2012-02-28 16:29:33 -0500

    Started PS4.

---
+++ PS4/problem1.m
@@ -0,0 +1,24 @@
+%Edward Venator
+%EECS 489 Spring 2012
+%Problem Set 4
+%Problem 1
+
+DH = [
+    0 pi/2 .5
+    0 -pi/2 0
+    0 pi/2 1
+    0 -pi/2 0
+    0 pi/2 1
+    0 -pi/2 0
+    0 0 1
+];
+
+payload_offs = [
+    0
+    .2
+    0
+];
+
+
+theta_a = zeros(7,1);
+theta_b = ones(7, 1);
