11da91c7228cdfad14c1e57a6d7188d791f4b3d3
Ed Venator <esv@case.edu>
2012-03-02 17:38:41 -0500

    Finished problem 1.

--- PS4/mass_jacobian.m
+++ PS4/mass_jacobian.m
@@ -1,6 +1,18 @@
-function [ J ] = mass_jacobian( pos_vec,  joint_number)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ J ] = mass_jacobian( p, z,  n)
+%mass_jacobian( pos_vec, joint_number)
+% Calculates the Jacobian for the mass at a given joint number
+% p is the list of all joint positions in the base/world coordinate
+% system
+% z is a list of all joint z-axis unit vectors in the base/world coordinate
+% system
+% n is the number of the joint to which the mass is fixed
 
+%Calculate Jacobian
+J = zeros(3, size(p,2));
+p_n = p(:,n+1);
+for i=1:n
+    %For a revolute joint
+    J(:,i) = cross(z(:,i), p_n-p(:,i));
+end
 
 end
--- PS4/problem1.m
+++ PS4/problem1.m
@@ -3,6 +3,14 @@
 %Problem Set 4
 %Problem 1
 
+%Given the masses of several joints, computes the torque on each joint to
+%hold a given set of joint angles.
+
+
+%Gravity
+g = [-9.8 0 0];
+
+%Denavit-Hartenberg Parameters
 DH = [
     0 pi/2 .5
     0 -pi/2 0
@@ -19,6 +27,51 @@ payload_offs = [
     0
 ];
 
+%Add payload to DH table as an eighth link
+DH(8,:) = [.2 0 0];
+%Also need to add pi/2 to theta 7
 
-theta_a = zeros(7,1);
-theta_b = ones(7, 1);
+%Masses
+m = [ 
+    100
+    0
+    50
+    0
+    25
+    0
+    0
+    .2 ];
+
+%Part A
+theta_a = zeros(8,1);
+theta_a(7) = pi/2;
+[~, p, z] = tooltransform(theta_a, DH);
+
+%For each joint j, calculate the sum of torques due to all masses i
+%There's a much more efficient way to do this, but this works
+tau_a = zeros(7,1);
+for j = 1:7
+    for i = 1:8
+        J = mass_jacobian(p, z, i);
+        tau_a(j) = tau_a(j) + m(i) * g * J(:,j);
+    end
+end
+
+tau_a
+
+
+%Part B
+theta_b = ones(8, 1);
+theta_b(7) = 1 + pi/2;
+[~, p, z] = tooltransform(theta_b, DH);
+
+%For each joint j, calculate the sum of torques due to all masses i
+tau_b = zeros(7,1);
+for j = 1:7
+    for i = 1:8
+        J = mass_jacobian(p, z, i);
+        tau_b(j) = tau_b(j) + m(i) * g * J(:,j);
+    end
+end
+
+tau_b
